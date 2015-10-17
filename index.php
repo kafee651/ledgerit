275c7ee5415af712b508282c106dd8e6885aa36b
Mohammad Kafee Uddin <kafee786@gmail.com>
2015-10-17 18:49:39 +0530

    path set

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-
 <head>
+	<title>My Expenses</title>
 
-<title>My Expenses</title>
+	<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+	<meta name="author" content="Erwin Aligam - styleshout.com" />
+	<meta name="description" content="Site Description Here" />
+	<meta name="keywords" content="keywords, here" />
+	<meta name="robots" content="index, follow, noarchive" />
+	<meta name="googlebot" content="noarchive" />
 
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
-<meta name="author" content="Erwin Aligam - styleshout.com" />
-<meta name="description" content="Site Description Here" />
-<meta name="keywords" content="keywords, here" />
-<meta name="robots" content="index, follow, noarchive" />
-<meta name="googlebot" content="noarchive" />
-
-<link rel="stylesheet" type="text/css" media="screen" href="../css/screen.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="../css/screen.css" />
 
 </head>
 
 <body>
-
 <!-- wrap starts here -->
 <div id="wrap">
 
