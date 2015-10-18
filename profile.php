301ba03321bed33da5b08701f933ec4c7cbd40e7
Mohammad Kafee Uddin <kafee786@gmail.com>
2015-10-18 12:25:35 +0530

    session file added

--- profile.php
+++ profile.php
@@ -44,7 +44,7 @@
 
    <tr>
    <td width="100%" align="center">
-<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+    <b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
 			<b id="logout"><a href="logout.php">Log Out</a></b>
 
    </td>
