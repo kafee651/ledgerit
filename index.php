7ddf7bbdba6a2502534a47868ebd37504fc42247
Mohammad Kafee Uddin <kafee651@users.noreply.github.com>
2017-12-10 18:02:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <?php
-	include('./login.php'); // Includes Login Script
+	include('login.php'); // Includes Login Script
 
 	if(isset($_SESSION['login_user'])){
-		header("location: ./profile.php");
+		header("location:profile.php");
 	}
 ?>
 <head>
