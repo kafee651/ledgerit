c6f428d446187585945da7d2a5971670a252d5ec
Mohammad Kafee Uddin <kafee651@users.noreply.github.com>
2017-12-10 18:40:11 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -19,35 +19,93 @@
 
 	</head>
 
-	<body>
+<body>
+<!-- wrap starts here -->
+<div id="wrap">
 
-		<!-- wrap starts here -->
-		<div id="wrap">
-
-		<!--header -->
-			<div id="header">			
+	<!--header -->
+	<div id="header">			
 				
-		<h1 id="logo-text"><a href="./index.php" title="">My Expenses</a></h1>		
+		<h1 id="logo-text"><a href="../new site/index.php" title="">My Expenses</a></h1>		
 		<p id="slogan">Use as Calculator... </p>	
-	
+		
+		<div  id="nav">
+			<ul>
+				<li><a href="../new site/insert.php">Insert</a></li>
+				<li><a href="../new site/search_date.php">Search</a></li>
+                <li class="first" id="current"><a href="../new site/create_user.php">Create New User</a></li>
+                <li><a href="../new site/login.php">My Account</a></li>
+                <li><a href="../new site/index.php">Logout</a></li>		
+			</ul>		
+		</div>	
+		
 		<div id="header-image"></div>
 						
 	<!--header ends-->					
 	</div>
+	<tr>
+   <td width="100%" align="center">
+	   <b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b></td>
+   <td width="100%" align="center">	   
+	   <b id="logout"><a href="./logout.php">Log Out</a></b>
+
+   </td>
+	   
+   <td width="100%" align="center">	   
+	   <b id="logout"><a href="./logout.php">Log Out</a></b>
+	   </td>
 	
 	<!-- featured starts -->
 	<!-- content -->
 	<div id="content-outer" class="clear"><div id="content-wrap">
 	
 		<div id="content">
-        <table width="880" border="0" cellspacing="0" cellpadding="0">
-
+        <table width="400" border="0" cellspacing="0" cellpadding="0">
    <tr>
-   <td width="100%" align="center">
-    <b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
-			<b id="logout"><a href="./logout.php">Log Out</a></b>
+   <td width="100%"><form action="../new site/create_user_comp.php" method="POST" >
+       <table width="430" height="415" border="0" align="left" >
+           <tr colspan=0>
+  	       <td align="Center" colspan="3"><h2 align="center">Create New User</h2></td></tr>
 
-   </td>
+        
+           
+           
+          
+           <tr>
+           <th width="156" scope="row"><p align="left">User Name<font color="#FF0000">*</font></p></th>
+           <td width="259"><font face='Verdana' size='2'>
+           <input size="50" maxlength="50" name="uname" type="text" id="name" /></font>
+           </td>
+           <td width="1"><span id="msgbox1"  ></span>
+           </td>
+           </tr>
+           <tr>
+           <th width="156" scope="row"><p align="left">User Id<font color="#FF0000">*</font></p></th>
+           <td width="259"><font face='Verdana' size='2'>
+           <input size="50" maxlength="50" name="userid" type="text" id="name" /></font>
+           </td>
+           <td width="1"><span id="msgbox1"  ></span>
+           </td>
+           </tr>
+           
+           <tr>
+           <th width="156" scope="row"><p align="left">Password<font color="#FF0000">*</font></p></th>
+           <td width="259"><font face='Verdana' size='2'>
+           <input size="50" maxlength="50" name="Password" type="password" id="name" /></font>
+           </td>
+           <td width="1"><span id="msgbox1"  ></span></td>
+           </tr>
+           <tr>
+           <th height="78" colspan="3" align="left" scope="row"><font color="#FF0000">*</font>All fields are compulsory<br/>
+           <center>
+           <input type="submit" />
+           &nbsp; &nbsp; &nbsp;
+           <input type="reset" />
+           </center>
+           <br/></th>
+           </tr>
+      </table>
+      </form></td>
     </tr>
 </table> 
 </div>	
@@ -61,13 +119,16 @@
 	<!-- footer-bottom starts -->		
 	<div id="footer-bottom">
 		<div class="bottom-left">
-			<p
-			&copy; 2011&nbsp; &nbsp;
-			Design by <a href="http://www.styleshout.com/">styleshout</a>
+			<p>
+			&copy; 2012 All rights are reserve&nbsp; &nbsp; &nbsp;
+			Design by <a href="http://www.styleshout.com/">kafee</a>
 			</p>
 		</div>
 	
 		<div class="bottom-right">
+			<p>		
+				<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | 
+		   	<a href="http://validator.w3.org/check/referer">XHTML</a>	|<a href="file:///C|/Users/KAIFE/Desktop/FreshPick10/index.html">SiLogin pages</a></p>
 		</div>
 	<!-- footer-bottom ends -->		
 	</div>
